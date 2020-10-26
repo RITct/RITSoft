a1410ad4dcd3746df429e6b46c50cb88a2bf88da
gautamkrishnar <r.gautamkrishna@gmail.com>
2020-10-26 16:03:59 +0530

    renamed debug env

--- ritsoftv2/header.php
+++ ritsoftv2/header.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 ob_start();
-error_reporting(getenv("MYSQL_USER") ? E_ALL : 0);
-ini_set('display_errors', getenv("MYSQL_USER") ? '1' : '0');
+error_reporting(getenv("DEBUG_APP") ? E_ALL : 0);
+ini_set('display_errors', getenv("DEBUG_APP") ? '1' : '0');
 ?>
 <!DOCTYPE html>
 <html lang="en">
