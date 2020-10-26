bfafcb688e815fa024a6711164fe2e2369bfc20d
gautamkrishnar <r.gautamkrishna@gmail.com>
2020-10-26 15:47:34 +0530

    Using php 5.4 for code compatibility

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
-FROM php:5.5-apache
-RUN docker-php-ext-install mysqli
+FROM php:5.4-apache
+RUN docker-php-ext-install mysql
--- ritsoftv2/connection.php
+++ ritsoftv2/connection.php
@@ -3,4 +3,5 @@ $host = getenv("MYSQL_HOST") ?: "127.0.0.1";
 $user = getenv("MYSQL_USER") ?: "ritsoftv2";
 $password = getenv("MYSQL_PASSWORD") ?: "ritsoftv2";
 $db = getenv("MYSQL_DB") ?: "ritsoftv2";
-$con=mysqli_connect($host, $user, $password, $db)or die("server not found");
+$con=mysql_connect($host, $user, $password, $db)or die("server not found");
+mysql_select_db("ritsoftv2",$con)or die("database not found");
